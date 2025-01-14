62433de309e81745e1cb059d0cbec08b90bbe3fc
Owen Hallock <owenhallock29@gmail.com>
2025-01-14 12:18:53 -0500

    Update Day07.java - do part1R next

--- src/main/java/Day07.java
+++ src/main/java/Day07.java
@@ -4,27 +4,60 @@ public class Day07 {
 
     private static final String FILE_PATH =
             "./input_files/day07ex.txt";
-            // "./input_files/day07p1.txt";
 
     public static void main(String[] args) {
         In in = new In(FILE_PATH); // input file
-        System.out.print(part1I(in));
-        System.out.print(part1R(in));
-        //System.out.print(part2(in));
+        System.out.println("Iterative result: " + part1I(in));
+;
     }
 
     public static long part1I(In in) {
+        long total = 0;
+
         while (in.hasNextLine()) {
-            String lineIn = in.readLine();
-            // TODO: the magic happens here...iteratively
+            String line = in.readLine();
+            String[] parts = line.split(": ");
+
+            long target = Long.parseLong(parts[0]);
+            String[] numStrings = parts[1].split(" ");
+
+            long[] numbers = new long[numStrings.length];
+            for (int i = 0; i < numStrings.length; i++) {
+                numbers[i] = Long.parseLong(numStrings[i]);
+            }
+
+            if (isParseable(numbers, target)) {
+                total += target;
+            }
         }
-        return 0;
+        return total;
+    }
+
+    private static boolean isParseable(long[] numbers, long target) {
+        int n = numbers.length;
+        int combinations = (int) Math.pow(2, n - 1); 
+
+        for (int i = 0; i < combinations; i++) {
+            long result = numbers[0];
+            for (int j = 0; j < n - 1; j++) {
+               
+                if ((i & (1 << j)) == 0) {
+                    result += numbers[j + 1];
+                } else {
+                    result *= numbers[j + 1];
+                }
+            }
+            if (result == target) {
+                return true; 
+            }
+        }
+        return false; 
     }
 
     public static long part1R(In in) {
         while (in.hasNextLine()) {
             String lineIn = in.readLine();
-            // TODO: the magic happens here...recursively
+            // TODO: the magic happens here...recursively. 
         }
         return 0;
     }
